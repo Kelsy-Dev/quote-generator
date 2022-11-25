df99d62118e9318e4bff96e7bb35efe903233a98
KLC <mkelsy@icloud.com>
2022-11-24 23:53:30 -0500

    adding js interactivity

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 //  Show new quotes
 function newQuotes() {
 	// Pick a random quote from apiQuotes array
 	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-	console.log(quote);
+	
+	if(!quote.author) {
+		authorText.textContent = 'Unknown';
+	} else {
+		authorText.textContent = quote.author;
+	}
+
+	if(quote.text.length > 50) {
+		quoteText.classList.add('long-quote');
+	} else {
+		quoteText.classList.remove('long-quote');
+	}
+	quoteText.textContent = quote.text;
 }
+
+// Post on Twitter
+function twitterPost() {
+	const newPost = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+	window.open(newPost, '_blank');
+}
+
+newQuoteBtn.addEventListener('click', newQuotes);
+twitterBtn.addEventListener('click', twitterPost);
 
 // Get Quotes from API
 async function getQuotes() {
